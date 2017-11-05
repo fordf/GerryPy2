110f5da9a44dfb094c1e8cdba24e53f24b195c17
Ford Fowler <fordjfowler@gmail.com>
2017-11-05 02:17:10 -0800

    repo init

---
+++ setup.py
@@ -0,0 +1,48 @@
+
+from setuptools import setup, find_packages
+
+# here = os.path.abspath(os.path.dirname(__file__))
+# with open(os.path.join(here, 'README.md')) as f:
+#     README = f.read()
+# with open(os.path.join(here, 'CHANGES.txt')) as f:
+#     CHANGES = f.read()
+
+requires = [
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'psycopg2',
+    'Flask',
+    'Flask-SocketIO',
+    'eventlet',
+    'GeoAlchemy2'
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    'tox',
+    'faker',
+    'pyramid_ipython',
+    'ipython'
+]
+
+setup(name='gerrypy2',
+      version='0.0',
+      description='Rebuild of GerryPy using websockets',
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Flask",
+      ],
+      author='Avery, Patrick, Jordan, Julien, Ford',
+      author_email='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      )
