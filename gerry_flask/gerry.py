3e9b1498f3a9e399fa5999d36ee7f36c9b1d5876
Ford Fowler <fordjfowler@gmail.com>
2017-11-16 22:35:28 -0800

    maybe it needs to be https!

--- gerry_flask/gerry.py
+++ gerry_flask/gerry.py
@@ -53,4 +53,9 @@ def client():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(
+        app,
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        ssl_context='adhoc'
+    )
