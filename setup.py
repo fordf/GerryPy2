f007a02245cf7091d29091e32268c88cb3b838f3
Ford Fowler <fordjfowler@gmail.com>
2017-11-16 20:49:31 -0800

    deployment files

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from setuptools import setup, find_packages
 # here = os.path.abspath(os.path.dirname(__file__))
 # with open(os.path.join(here, 'README.md')) as f:
 #     README = f.read()
-# with open(os.path.join(here, 'CHANGES.txt')) as f:
-#     CHANGES = f.read()
 
 requires = [
     'SQLAlchemy',
@@ -19,15 +17,15 @@ requires = [
     'GeoAlchemy2'
 ]
 
-tests_require = [
-    'WebTest >= 1.3.1',  # py3 compat
-    'pytest',  # includes virtualenv
-    'pytest-cov',
-    'tox',
-    'faker',
-    'pyramid_ipython',
-    'ipython'
-]
+# tests_require = [
+#     'WebTest >= 1.3.1',  # py3 compat
+#     'pytest',  # includes virtualenv
+#     'pytest-cov',
+#     'tox',
+#     'faker',
+#     'pyramid_ipython',
+#     'ipython'
+# ]
 
 setup(name='gerrypy2',
       version='0.0',
@@ -41,8 +39,8 @@ setup(name='gerrypy2',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      extras_require={
-          'testing': tests_require,
-      },
+      # extras_require={
+      #     'testing': tests_require,
+      # },
       install_requires=requires,
       )
